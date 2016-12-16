bdb6dc0c25cd8aa3517b69f424337557c297ae23
Eben <me@ebenroux.co.za>
2016-12-16 21:45:24 +0200

    - minor refactoring

--- Shuttle.Core.AutoFac.Tests/AutoFacComponentContainerFixture.cs
+++ Shuttle.Core.AutoFac.Tests/AutoFacComponentContainerFixture.cs
@@ -2,13 +2,26 @@
 using NUnit.Framework;
 using Shuttle.Core.AutoFac;
 using Shuttle.Core.ComponentContainer.Tests;
-using Shuttle.Core.Infrastructure;
 
 namespace Shuttle.Core.Castle.Tests
 {
     [TestFixture]
     public class AutoFacComponentContainerFixture : ComponentContainerFixture
     {
+        [Test]
+        public void Should_be_able_resolve_all_instances()
+        {
+            var containerBuilder = new ContainerBuilder();
+
+            var registry = new AutoFacComponentRegistry(containerBuilder);
+
+            RegisterCollection(registry);
+
+            var resolver = new AutoFacComponentResolver(containerBuilder.Build());
+
+            ResolveCollection(resolver);
+        }
+
         [Test]
         public void Should_be_able_to_register_and_resolve_a_singleton()
         {
@@ -35,21 +48,6 @@ namespace Shuttle.Core.Castle.Tests
             var resolver = new AutoFacComponentResolver(containerBuilder.Build());
 
             ResolveTransient(resolver);
-        }
-
-
-        [Test]
-        public void Should_be_able_resolve_all_instances()
-        {
-            var containerBuilder = new ContainerBuilder();
-
-            var registry = new AutoFacComponentRegistry(containerBuilder);
-
-            RegisterCollection(registry);
-
-            var resolver = new AutoFacComponentResolver(containerBuilder.Build());
-
-            ResolveCollection(resolver);
         }
     }
 }

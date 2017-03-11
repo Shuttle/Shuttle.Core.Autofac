85a8d9f22a3684db7b8e4d97c964d5ff878276ad
Eben <me@ebenroux.co.za>
2017-03-11 11:13:37 +0200

    - multiple instances

--- Shuttle.Core.AutoFac.Tests/AutoFacComponentContainerFixture.cs
+++ Shuttle.Core.AutoFac.Tests/AutoFacComponentContainerFixture.cs
@@ -1,56 +1,81 @@
-﻿using System;
-using System.Linq.Expressions;
-using Autofac;
+﻿using Autofac;
 using NUnit.Framework;
 using Shuttle.Core.Autofac;
 using Shuttle.Core.ComponentContainer.Tests;
-using Shuttle.Core.Infrastructure;
 
 namespace Shuttle.Core.Castle.Tests
 {
 	[TestFixture]
-    public class AutofacComponentContainerFixture : ComponentContainerFixture
-    {
-        [Test]
-        public void Should_be_able_resolve_all_instances()
-        {
-            var containerBuilder = new ContainerBuilder();
+	public class AutofacComponentContainerFixture : ComponentContainerFixture
+	{
+		[Test]
+		public void Should_be_able_resolve_all_instances()
+		{
+			var containerBuilder = new ContainerBuilder();
 
-            var registry = new AutofacComponentRegistry(containerBuilder);
+			var registry = new AutofacComponentRegistry(containerBuilder);
 
-            RegisterCollection(registry);
+			RegisterCollection(registry);
 
-            var resolver = new AutofacComponentResolver(containerBuilder.Build());
+			var resolver = new AutofacComponentResolver(containerBuilder.Build());
 
-            ResolveCollection(resolver);
-        }
+			ResolveCollection(resolver);
+		}
 
-        [Test]
-        public void Should_be_able_to_register_and_resolve_a_singleton()
-        {
-            var containerBuilder = new ContainerBuilder();
+		[Test]
+		public void Should_be_able_to_register_and_resolve_a_multiple_singleton()
+		{
+			var containerBuilder = new ContainerBuilder();
 
-            var registry = new AutofacComponentRegistry(containerBuilder);
+			var registry = new AutofacComponentRegistry(containerBuilder);
 
-            RegisterSingleton(registry);
+			RegisterMultipleSingleton(registry);
 
-            var resolver = new AutofacComponentResolver(containerBuilder.Build());
+			var resolver = new AutofacComponentResolver(containerBuilder.Build());
 
-            ResolveSingleton(resolver);
-        }
+			ResolveMultipleSingleton(resolver);
+		}
 
-        [Test]
-        public void Should_be_able_to_register_and_resolve_transient_components()
-        {
-            var containerBuilder = new ContainerBuilder();
+		[Test]
+		public void Should_be_able_to_register_and_resolve_a_singleton()
+		{
+			var containerBuilder = new ContainerBuilder();
 
-            var registry = new AutofacComponentRegistry(containerBuilder);
+			var registry = new AutofacComponentRegistry(containerBuilder);
 
-            RegisterTransient(registry);
+			RegisterSingleton(registry);
 
-            var resolver = new AutofacComponentResolver(containerBuilder.Build());
+			var resolver = new AutofacComponentResolver(containerBuilder.Build());
 
-            ResolveTransient(resolver);
-        }
-    }
+			ResolveSingleton(resolver);
+		}
+
+		[Test]
+		public void Should_be_able_to_register_and_resolve_multiple_transient_components()
+		{
+			var containerBuilder = new ContainerBuilder();
+
+			var registry = new AutofacComponentRegistry(containerBuilder);
+
+			RegisterMultipleTransient(registry);
+
+			var resolver = new AutofacComponentResolver(containerBuilder.Build());
+
+			ResolveMultipleTransient(resolver);
+		}
+
+		[Test]
+		public void Should_be_able_to_register_and_resolve_transient_components()
+		{
+			var containerBuilder = new ContainerBuilder();
+
+			var registry = new AutofacComponentRegistry(containerBuilder);
+
+			RegisterTransient(registry);
+
+			var resolver = new AutofacComponentResolver(containerBuilder.Build());
+
+			ResolveTransient(resolver);
+		}
+	}
 }

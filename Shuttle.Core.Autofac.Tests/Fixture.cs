db5cba440673051ba6694883ba31dd2e1df27cac
eben-roux <me@ebenroux.co.za>
2017-12-28 13:54:54 +0200

    - refactoring

---
+++ Shuttle.Core.Autofac.Tests/Fixture.cs
@@ -0,0 +1,80 @@
+﻿using Autofac;
+using NUnit.Framework;
+using Shuttle.Core.Container.Tests;
+
+namespace Shuttle.Core.Autofac.Tests
+{
+	[TestFixture]
+	public class Fixture : ContainerFixture
+	{
+		[Test]
+		public void Should_be_able_resolve_all_instances()
+		{
+			var containerBuilder = new ContainerBuilder();
+
+			var registry = new AutofacComponentRegistry(containerBuilder);
+
+			RegisterCollection(registry);
+
+			var resolver = new AutofacComponentResolver(containerBuilder.Build());
+
+			ResolveCollection(resolver);
+		}
+
+		[Test]
+		public void Should_be_able_to_register_and_resolve_a_singleton()
+		{
+			var containerBuilder = new ContainerBuilder();
+
+			var registry = new AutofacComponentRegistry(containerBuilder);
+
+			RegisterSingleton(registry);
+
+			var resolver = new AutofacComponentResolver(containerBuilder.Build());
+
+			ResolveSingleton(resolver);
+		}
+
+		[Test]
+		public void Should_be_able_to_register_and_resolve_transient_components()
+		{
+			var containerBuilder = new ContainerBuilder();
+
+			var registry = new AutofacComponentRegistry(containerBuilder);
+
+			RegisterTransient(registry);
+
+			var resolver = new AutofacComponentResolver(containerBuilder.Build());
+
+			ResolveTransient(resolver);
+		}
+
+		[Test]
+		public void Should_be_able_to_register_and_resolve_a_multiple_singleton()
+		{
+			var containerBuilder = new ContainerBuilder();
+
+			var registry = new AutofacComponentRegistry(containerBuilder);
+
+			RegisterMultipleSingleton(registry);
+
+			var resolver = new AutofacComponentResolver(containerBuilder.Build());
+
+			ResolveMultipleSingleton(resolver);
+		}
+
+		[Test]
+		public void Should_be_able_to_register_and_resolve_multiple_transient_components()
+		{
+			var containerBuilder = new ContainerBuilder();
+
+			var registry = new AutofacComponentRegistry(containerBuilder);
+
+			RegisterMultipleTransient(registry);
+
+			var resolver = new AutofacComponentResolver(containerBuilder.Build());
+
+			ResolveMultipleTransient(resolver);
+		}
+	}
+}
